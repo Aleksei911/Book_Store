2e57a6c48c974758f00723b2a18aae8fea0e53e7
Aleksei Kohnuk <kohnuk92@gmail.com>
2023-03-14 12:08:43 +0300

    update tests, add caching fields

--- books/store/tests/test_api.py
+++ books/store/tests/test_api.py
@@ -25,8 +25,7 @@ class BooksApiTestCase(APITestCase):
             response = self.client.get(url)
             self.assertEqual(2, len(queries))
         books = Book.objects.all().annotate(
-            annotated_likes=Count(Case(When(userbookrelation__like=True, then=1))),
-            rating=Avg('userbookrelation__rate')
+            annotated_likes=Count(Case(When(userbookrelation__like=True, then=1)))
         ).order_by('id')
 
         serializer_data = BookSerializer(books, many=True).data
@@ -38,8 +37,7 @@ class BooksApiTestCase(APITestCase):
     def test_get_filter(self):
         url = reverse('book-list')
         books = Book.objects.filter(id__in=[self.book_1.id, self.book_3.id]).annotate(
-            annotated_likes=Count(Case(When(userbookrelation__like=True, then=1))),
-            rating=Avg('userbookrelation__rate')
+            annotated_likes=Count(Case(When(userbookrelation__like=True, then=1)))
         ).order_by('id')
 
         response = self.client.get(url, data={'search': 'Author 1'})
